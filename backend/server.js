b03209fe131d6cec61b673b28b213b426b349b16
isahilmishra <isahilmishra0175@gmail.com>
2026-06-16 03:30:10 +0530

    chore: setup cors and dynamic api url for production

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, 'http://localhost:5173', 'http://127.0.0.1:5173'] : ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use(express.json());
